c573125db6d064ac874fa028502ca86892f4ac7f
valyafrontend1989 <144459911+valyafrontend1989@users.noreply.github.com>
2025-10-19 15:19:12 +0300

    chore: done 3 tasks

---
+++ js/task-1.js
@@ -0,0 +1,24 @@
+function isEnoughCapacity(products, containerSize) {
+  let sumProducts = 0;
+  for (const key in products) {
+    sumProducts = sumProducts + products[key];
+  }
+
+  return containerSize >= sumProducts;
+}
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+
+// Залиш цей код для перевірки ментором.
+
+// На що буде звертати увагу ментор при перевірці:
+// Оголошена функція isEnoughCapacity(products, containerSize)
+// Виклик isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8) повертає true
+// Виклик isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12) повертає false
+// Виклик isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14) повертає true
+// Виклик isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7) повертає false
